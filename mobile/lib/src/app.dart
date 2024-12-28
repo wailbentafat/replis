3f3da883ec5f295bbbcadf4c5b191d3051a81cc1
bentafat wail <150479778+wailbentafat@users.noreply.github.com>
2024-12-28 23:17:27 +0100

    init

---
+++ mobile/lib/src/app.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+class myapp extends StatefulWidget {
+  const myapp({super.key});
+
+  @override
+  State<myapp> createState() => _myappState();
+}
+
+class _myappState extends State<myapp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      highContrastDarkTheme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+      ),
+      // home: const loginpage(),
+    );
+  }
+}
---
+++ mobile/lib/src/presentation/screens/loginpage.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../view_models/login_view_models.dart';
+
+class loginpage extends StatelessWidget {
+  const loginpage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(backgroundColor: Color(0xFF1C4E80),
+      body: Consumer<LoginViewModels>(builder: (context, LoginViewModels, child) {
+      return Column(
+        mainAxisAlignment: ,
+
+      );
+    }));
+  }
+}
